23728eb65dffe10728d199f819609d05e9425f3e
Fiammamuscari <fiammamuscari@gmail.com>
2024-05-06 02:01:20 -0300

    fix: sticker dimentions

--- src/Components/ImageComponent.js
+++ src/Components/ImageComponent.js
@@ -118,6 +118,8 @@ const ImageComponent = ({
         shadowOffsetX={0}
         shadowOffsetY={4}
         shadowOpacity={0.6}
+        width={100}
+        height={100}
         onTransformEnd={handleTransformOnEnd}
       />
       {isSelected && (
